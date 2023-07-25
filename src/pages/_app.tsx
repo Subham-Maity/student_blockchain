3cc8923c1d9c90aac9c99a87f48becb4ba79526b
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-07-25 13:42:45 +0530

    Dashboard Protected

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import {AuthContextProvider} from '@/context/AuthContext'
+import { useRouter } from 'next/router'
 import Navbar from "@/components/Navbar";
+import ProtectedRoute from "@/components/ProtectedRoute";
+const noAuthRequired = ['/', '/login', '/signup']
 
 export default function App({Component, pageProps}: AppProps) {
+    const router = useRouter()
     return (<AuthContextProvider>
         <Navbar/>
-        <Component {...pageProps} />
+        {noAuthRequired.includes(router.pathname) ? (
+            <Component {...pageProps} />
+        ) : (
+            <ProtectedRoute>
+                <Component {...pageProps} />
+            </ProtectedRoute>
+        )}
     </AuthContextProvider>)
 }
