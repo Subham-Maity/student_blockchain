c7657ac75d03b679ede52d79fd9d98414e1934d9
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-07-25 11:53:05 +0530

    Auth Context Setup

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-
+import { getAuth } from 'firebase/auth'
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_apiKey,
     authDomain: process.env.NEXT_PUBLIC_authDomain,
@@ -11,4 +11,4 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-
+export const auth = getAuth()
